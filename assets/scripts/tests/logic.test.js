15cd03f9a4db5b7116a9121da5fbb82cf1cfcfcc
cjns <67065839+cjns@users.noreply.github.com>
2023-08-20 17:47:14 +0100

    Test shuffle function

--- assets/scripts/tests/logic.test.js
+++ assets/scripts/tests/logic.test.js
@@ -27,4 +27,20 @@ describe('shuffle function', () => {
     const shuffled = shuffle(inputArray);
     expect(shuffled.length).toBe(inputArray.length);
   });
+
+  it('should return an array with the same elements as the input array', () => {
+    const inputArray = [1, 2, 3, 4, 5];
+    const shuffled = shuffle(inputArray);
+    inputArray.forEach(item => {
+      expect(shuffled).toContain(item);
+    });
+  });
+
+  it('should not always return the same order for multiple calls', () => {
+    const inputArray = [1, 2, 3, 4, 5];
+    const firstShuffle = shuffle([...inputArray]);
+    const secondShuffle = shuffle([...inputArray]);
+    expect(firstShuffle).not.toEqual(secondShuffle);
+  });
+
 });
